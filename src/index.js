7932f452d479311127e7145e1d44db39189b7d6b
Lukas Kusik <lukas.kusik@gmail.com>
2020-07-26 08:56:40 +0200

    Remove mongodb-runner + update to newest

--- src/index.js
+++ src/index.js
@@ -1,51 +1,24 @@
 const Promise = require('bluebird');
 const express = require('express');
 const http = require('http');
-const {MongoClient} = require('mongodb');
-const {ParseServer} = require('parse-server');
+const { MongoClient } = require('mongodb');
+const { ParseServer } = require('parse-server');
 
-const mongoDBRunnerStart = require('mongodb-runner/mocha/before').bind({
-  timeout() {
-  },
-  slow() {
-  },
-});
-const mongoDBRunnerStop = require('mongodb-runner/mocha/after');
 
-const startDB = () => (
-  new Promise((done, reject) => {
-    done.fail = reject;
-    mongoDBRunnerStart(done);
-  })
-);
-
-const stopDB = () => (
-  new Promise((done, reject) => {
-    done.fail = reject;
-    mongoDBRunnerStop(done);
-  })
-);
-
-const connectDB = (databaseURI) => new Promise((resolve, reject) => {
-  MongoClient.connect(databaseURI, (err, db) => {
-    if (err) {
-      reject(err);
-    } else {
-      resolve(db);
-    }
-  });
-});
+async function connectDB(databaseURI) {
+    return MongoClient.connect(databaseURI, { useUnifiedTopology: true });
+}
 
 let parseServerState = {};
 
 const dropDB = () => {
-  const {mongoConnection} = parseServerState;
-  return new Promise((resolve, reject) => {
-    mongoConnection.dropDatabase((err) => {
-      if (err) return reject(err);
-      resolve();
+    const { mongoConnection } = parseServerState;
+    return new Promise((resolve, reject) => {
+        mongoConnection.db().dropDatabase((err) => {
+            if (err) return reject(err);
+            resolve();
+        });
     });
-  });
 };
 
 /**
@@ -53,46 +26,44 @@ const dropDB = () => {
  * @param {Object} parseServerOptions Used for creating the `ParseServer`
  * @return {Promise} Runner state
  */
-function startParseServer(parseServerOptions = {}) {
-  const mongodbPort = process.env.MONGODB_PORT || 27017;
-  const {
-    databaseName = 'parse-test',
-    databaseURI = `mongodb://localhost:${mongodbPort}/${databaseName}`,
-    masterKey = 'test',
-    javascriptKey = 'test',
-    appId = 'test',
+async function startParseServer(parseServerOptions = {}) {
+    const mongodbPort = process.env.MONGODB_PORT || 27017;
+    const {
+        databaseName = 'parse-test',
+        databaseURI = `mongodb://localhost:${mongodbPort}/${databaseName}`,
+        masterKey = 'test',
+        javascriptKey = 'test',
+        appId = 'test',
 
-    port = 30001,
-    mountPath = '/1',
-    serverURL = `http://localhost:${port}${mountPath}`,
-  } = parseServerOptions;
+        port = 30001,
+        mountPath = '/1',
+        serverURL = `http://localhost:${port}${mountPath}`,
+    } = parseServerOptions;
 
-  return startDB()
-    .then(() => connectDB(databaseURI))
-    .then((mongoConnection) => {
-      parseServerOptions = Object.assign({
+    mongoConnection = await connectDB(databaseURI);
+
+    parseServerOptions = Object.assign({
         masterKey, javascriptKey, appId,
         serverURL,
         databaseURI,
         silent: process.env.VERBOSE !== '1',
-      }, parseServerOptions);
-      const app = express();
-      const parseServer = new ParseServer(parseServerOptions);
+    }, parseServerOptions);
+    const app = express();
+    const parseServer = new ParseServer(parseServerOptions);
 
-      app.use(mountPath, parseServer);
+    app.use(mountPath, parseServer);
 
-      const httpServer = http.createServer(app);
+    const httpServer = http.createServer(app);
 
-      Promise.promisifyAll(httpServer);
+    Promise.promisifyAll(httpServer);
 
-      return httpServer.listenAsync(port)
-        .then(() => Object.assign(parseServerState, {
-          parseServer,
-          httpServer,
-          mongoConnection,
-          expressApp: app,
-          parseServerOptions,
-        }));
+    await httpServer.listenAsync(port)
+    Object.assign(parseServerState, {
+        parseServer,
+        httpServer,
+        mongoConnection,
+        expressApp: app,
+        parseServerOptions,
     });
 }
 
@@ -101,17 +72,15 @@ function startParseServer(parseServerOptions = {}) {
  * @return {Promise}
  */
 function stopParseServer() {
-  const {httpServer} = parseServerState;
-  return httpServer.closeAsync()
-    .then(stopDB)
-    .then(() => parseServerState = {});
+    const { httpServer } = parseServerState;
+    return httpServer.closeAsync()
+        .then(() => parseServerState = {});
 }
 
 module.exports = {
-  dropDB,
-  startParseServer,
-  stopParseServer,
-
-  parseServerState,
+    dropDB,
+    startParseServer,
+    stopParseServer,
+    parseServerState,
 };
 
